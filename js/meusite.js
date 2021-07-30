9bbffe1c19f8cc7707bc4a386fb191cda725b6cd
fsilva19 <67085132+fsilva19@users.noreply.github.com>
2021-07-30 00:39:31 -0300

    Add files via upload

--- js/meusite.js
+++ js/meusite.js
@@ -1,9 +1,12 @@
 var clique_inicio = document.getElementById("inicio");
 
+
 function clique() {
     clique_inicio.style.backgroundImage = "linear-gradient(to right, #FD5341 ,#7A0B00)";
+    clique_inicio.style.color = "white";
 }
 
 function solte() {
     clique_inicio.style.backgroundImage = "linear-gradient(to right, #00c1ff, #228aab)";
+    clique_inicio.style.color = "black";
 }
